908dda54a60c548f141d208a02ea7d5029c90e85
TK421bsod <colin@kolya.org>
2020-05-20 16:13:40 -0700

    finally starting to rewrite the bot, added logging

--- maximillian-bot-stable.py
+++ maximillian-bot-stable.py
@@ -2,11 +2,14 @@ import discord
 from discord.ext import commands 
 from common import db
 from common import token
+import logging
 
+logging.basicConfig(level=logging.INFO)
 tokeninst = token()
 dbinst = db()
 client = discord.Client()
 bot = discord.ext.commands()
 decrypted_token = tokeninst.decrypt()
+
 
 client.run(decrypted_token.decode())
