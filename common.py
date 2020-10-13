d88352c9abbb4044edbf759fe6d4a8058daaa897
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2020-10-12 22:10:32 -0700

    add exec_query function

--- common.py
+++ common.py
@@ -128,6 +128,13 @@ class db:
             return "successful"
         else:
             return "error"
+    def exec_query(self, database, querytoexecute, debug):
+        self.connect(database)
+        self.dbc.execute(str(querytoexecute))
+        row = self.dbc.fetchone()
+        if debug == True:
+            print(str(row))
+        return row
 
 class token:
     def decrypt(self, filename):
