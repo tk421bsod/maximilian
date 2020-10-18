fef513a488d9adeb04e81502c62aea465c1c9bdb
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2020-10-18 00:01:08 -0700

    revert to using = instead of like

--- common.py
+++ common.py
@@ -106,7 +106,7 @@ class db:
     
     def retrieve(self, database, table, valuetoretrieve, valuenametoretrieve,  retrievedvalue, debug):
         self.connect(database)
-        self.dbc.execute("select {} from {} where {} like %s".format(valuetoretrieve, table, valuenametoretrieve), (retrievedvalue))
+        self.dbc.execute("select {} from {} where {} = %s".format(valuetoretrieve, table, valuenametoretrieve), (retrievedvalue))
         row = self.dbc.fetchone()
         if debug == True:
             print("Value to retrieve: " + str(valuetoretrieve))
