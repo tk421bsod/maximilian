c5e898ec4419ec986e003415050ea2db0e1a8d83
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2020-10-30 21:04:57 -0700

    change format of changing prefix message

--- maximillian-bot-stable.py
+++ maximillian-bot-stable.py
@@ -93,7 +93,7 @@ async def prefix(ctx, arg):
     #should probably make this shorter and eliminate a bunch of those if statements
     if ctx.author.guild_permissions.administrator or ctx.author.id == bot.owner_id:
         print("changing prefix...")
-        changingprefixmessage = await ctx.send("Ok. Changing prefix...")
+        changingprefixmessage = await ctx.send("Ok. Changing prefix to `" + arg +"`...")
         start_time = time.time()
         await ctx.trigger_typing()
         prefixsetmessage = "My prefix in this server has been set to `" + str(arg) + "` ."
@@ -347,7 +347,6 @@ async def about(ctx):
     embed.add_field(name="Useful links", value="Use `" + str(bot.command_prefix) + "help command` for more info on a certain command. \n For more help, join the support server at https://discord.gg/PJ94gft. \n To add Maximilian to your server, with only the required permissions, click [here](https://discord.com/api/oauth2/authorize?client_id=620022782016618528&permissions=268815456&scope=bot).", inline=False)
     embed.add_field(name="Fun Commands", value="Commands that have no purpose. \n `zalgo` `cats` `ping`", inline=True)
     embed.add_field(name="Other Commands", value="Commands that actually have a purpose. \n `about` `help` `userinfo` `reactionroles` `responses` `prefix` `listprefixes` `hi`", inline=True)
-    embed.set_footer(text="maximilian-rewrite version 3.1")
     await ctx.send(embed=embed)
 
 @commands.is_owner()
@@ -362,6 +361,8 @@ async def reload_utils(ctx):
     await reloadmessage.edit(content="Reloaded utilities!")
     await exectime(start_time, ctx)
 
+@commands.has_permissions(perms=discord.Permissions.manage_guild)
+@bot.command(help="")
 
 print("starting bot")
 bot.run(decrypted_token)
