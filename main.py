e6858da7a11a1c3de476d18bb26d2c84eff1e409
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2020-11-03 11:52:03 -0800

    use new help command

--- main.py
+++ main.py
@@ -32,6 +32,60 @@ bot.miscinst = bot.get_cog('misc')
 bot.reactionrolesinst = bot.get_cog('reactionroles')
 print('loaded extensions, waiting for on-ready')
 
+class HelpCommand(commands.HelpCommand):
+    color = discord.Colour.blurple()
+    def get_ending_note(self):
+        return 'Use {0}{1} [command] for more info on a command.'.format(self.clean_prefix, self.invoked_with)
+
+    def get_command_signature(self, command):
+        return '{0.qualified_name} {0.signature}'.format(command)
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title='Commands', colour=self.color)
+        description = self.context.bot.description
+        if description:
+            embed.description = description
+
+        for cog, commands in mapping.items():
+            if cog is not None:
+                name = cog.qualified_name
+                filtered = await self.filter_commands(commands, sort=True)
+                if filtered:
+                    value = '\u2002 '.join('`' + c.name + '`' for c in commands)
+                    if cog and cog.description:
+                        value = '{0}\n{1}'.format(cog.description, value)
+
+                    embed.add_field(name=name, value=value)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    async def send_cog_help(self, cog):
+        embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog), colour=self.color)
+        if cog.description:
+            embed.description = cog.description
+
+        filtered = await self.filter_commands(cog.get_commands(), sort=True)
+        for command in filtered:
+            embed.add_field(name=self.get_command_signature(command), value=command.short_doc or '...', inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    async def send_group_help(self, group):
+        embed = discord.Embed(title=group.qualified_name, colour=self.color)
+        if group.help:
+            embed.description = group.help
+
+        if isinstance(group, commands.Group):
+            filtered = await self.filter_commands(group.commands, sort=True)
+            for command in filtered:
+                embed.add_field(name=self.get_command_signature(command), value=command.short_doc or '...', inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+    send_command_help = send_group_help
+
 async def startup():
     await bot.wait_until_ready()
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=str(len(bot.guilds))+" guilds!"))
@@ -40,6 +94,7 @@ async def startup():
 async def on_ready():
     await bot.prefixesinst.reset_prefixes()
     await bot.responsesinst.get_responses()
+    bot.help_command = HelpCommand()
     bot.loop.create_task(startup())
     print("ready")
     
