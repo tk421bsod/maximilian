bac7b84bab4d4de89670f3b2efaf845af5f0b4bf
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2020-11-04 16:09:20 -0800

    change required permissions

--- misc.py
+++ misc.py
@@ -33,7 +33,7 @@ class misc(commands.Cog):
     @commands.command(help="Get some info about the bot and commands")
     async def about(self, ctx):
         embed = discord.Embed(title="About", color=discord.Color.blurple())
-        embed.add_field(name="Useful links", value=f"Use `{str(self.bot.command_prefix)}help command` for more info on a certain command. \n For more help, join the support server at https://discord.gg/PJ94gft. \n To add Maximilian to your server, with only the required permissions, click [here](https://discord.com/api/oauth2/authorize?client_id=620022782016618528&permissions=268815456&scope=bot).", inline=False)
+        embed.add_field(name="Useful links", value=f"Use `{str(self.bot.command_prefix)}help command` for more info on a certain command. \n For more help, join the support server at https://discord.gg/PJ94gft. \n To add Maximilian to your server, with only the required permissions, click [here](https://discord.com/api/oauth2/authorize?client_id=620022782016618528&permissions=268797952&scope=bot).", inline=False)
         embed.add_field(name="Fun Commands", value="Commands that have no purpose. \n `zalgo` `cats` `ping`", inline=True)
         embed.add_field(name="Other Commands", value="Commands that actually have a purpose. \n `about` `help` `userinfo` `reactionroles` `responses` `prefix` `listprefixes` `hi`", inline=True)
         await ctx.send(embed=embed)
