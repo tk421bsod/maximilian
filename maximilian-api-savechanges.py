24743e82cfc0b0c8022d38e0c1af1904576b9e8a
TK421bsod <colin@kolya.org>
2020-12-06 17:20:10 -0800

    migrate api to aiohttp

--- maximilian-api-savechanges.py
+++ maximilian-api-savechanges.py
@@ -1,48 +1,34 @@
-from flask import Flask, escape, request, redirect
+from aiohttp import web
 import datetime
 import pymysql.cursors
 from cryptography.fernet import Fernet
 import os
 from common import db
-import requests
 
 dbinst = db()
-dbinst.connect("maximilian")
-#once imported, open the log file in append mode, as we don't want to overwrite the file every time this is ran
-log = open("maximilian-api-savechanges-log.txt", "a")
-#write the time the log file was opened and flush the buffer so changes appear immediately
-log.write("Log opened at " + str(datetime.datetime.now()) + ". \n")
-log.flush()
+dbinst.connect("maximilian")    
 
-app = Flask('maximilian-api-savechanges')
+routes = web.RouteTableDef()
 
-@app.route('/other-projects/maximilian/api', methods=['GET', 'POST'])
-def save():
+@routes.get('/other-projects/maximilian/api')
+async def save(request):
     try:
-        #this recieves requests originating from the save function in common.js
-        #data gets passed to this as parameters in a url, and this concatenates a dict containing the values and value names and passes all of the data to the insert function of common.py
         print("Request recieved")
         values = {}
         #TODO: Use the 'logging' module for logging, as it simplifies this
-        log.write("Request recieved at " + str(datetime.datetime.now()) + ". Processing request... \n")
-        log.flush()
-        log.write("Getting parameters from URL and concatenating dict from them... \n")
-        log.flush()
         print("getting parameters...")
-        valuenodupe = request.args.get('valuenodupe', '')
-        table = request.args.get('table', '')
-        path = request.args.get('path', '')
-        database = request.args.get('database', '')
-        debug = bool(request.args.get('debug', False))
-        valueallnum = request.args.get('valueallnum', '')
-        valueallnumenabled = bool(request.args.get('valueallnumenabled', 'false'))
-        currentdomain = request.args.get('currentdomain', 'animationdoctorstudio.net')
+        valuenodupe = request.rel_url.query.get('valuenodupe', '')
+        table = request.rel_url.query.get('table', '')
+        path = request.rel_url.query.get('path', '')
+        database = request.rel_url.query.get('database', '')
+        debug = bool(request.rel_url.query.get('debug', False))
+        valueallnum = request.rel_url.query.get('valueallnum', '')
+        valueallnumenabled = bool(request.rel_url.query.get('valueallnumenabled', 'false'))
+        currentdomain = request.rel_url.query.get('currentdomain', 'animationdoctorstudio.net')
         print("appending values to dict of values")
-        for key, value in request.args.items():
+        for key, value in request.rel_url.query.items():
             if value != valuenodupe and value != table and value != path and value != debug and value != database and value != valueallnum and value != str(valueallnumenabled) and value != currentdomain:
                 values[key] = value
-        log.write("Finished getting parameters. Inserting data... \n")
-        log.flush()
         print(currentdomain)
         if debug == True:
             print(valuenodupe)
@@ -58,15 +44,13 @@ def save():
         results = {"success":"?redirectsource=savechanges&changessaved=success", "debuginfoprinted":"", "error-duplicate":"?redirectsource=savechanges&changessaved=error-duplicate", "error-unhandled":"?redirectsource=savechanges&changessaved=error-other&error="+dbinst.error+"&errorlocation=common-py-inserting-data", "error-valuenotallnum":"?redirectsource=savechanges&changessaved=error-valuenotallnum"}
         for key in results.keys():
             if result == key:
-                return redirect('http://' + currentdomain + '/other-projects/maximilian/' + path + results[result])
-        return redirect('http://' + currentdomain + '/other-projects/maximilian/' + path + "?redirectsource=savechanges&changessaved=error-other&error="+ result +"&errorlocation=common-py-inserting-data")
-        
+                return web.HTTPFound('http://' + currentdomain + '/other-projects/maximilian/' + path + results[result])
+        return web.HTTPFound('http://' + currentdomain + '/other-projects/maximilian/' + path + "?redirectsource=savechanges&changessaved=error-other&error="+ result +"&errorlocation=common-py-inserting-data")
     except Exception as e:
-        print("Error: " + str(e) + ". Check the log file for more details.")
-        log.write("Error: " + str(e) + ". \n")
-        log.flush()
-        return redirect('http://' + currentdomain + '/other-projects/maximilian/' + path + '?redirectsource=savechanges&changesaved=error-other&error=\''+str(e)+'\'&errorlocation=savechanges-api')
+        print("Error: " + str(e) + ".")
+        return web.HTTPFound('http://' + currentdomain + '/other-projects/maximilian/' + path + '?redirectsource=savechanges&changesaved=error-other&error=\''+str(e)+'\'&errorlocation=savechanges-api')
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app, port=5000)
