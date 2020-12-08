ebbae38fc91c22dd44009af3c47189d958fb6d11
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2020-12-08 08:26:34 -0800

    move privacy commands to misc.py, add help entries

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import calendar
 import os
 import sys
 import git 
-import asyncio
 import pymysql
 
 #set up logging
@@ -260,42 +259,6 @@ async def change_status(ctx, type, newstatus):
         await ctx.send("That's an invalid status type!")
         return
     await ctx.send("Changed status!")
-
-@bot.command(hidden=True)
-async def privacy(ctx):
-    embed = discord.Embed(title="Maximilian Privacy Policy", color=discord.Color.blurple())
-    embed.add_field(name="Why Maximilian collects data", value="Maximilian collects certain information that is necessary for its functions (types of data collected are described below)", inline=False)
-    embed.add_field(name="Data that Maximilian collects", value="**-Server IDs**\nMaximilian collects server IDs when you create a custom command or add a reaction role to distinguish between different servers.\n\n**-Role IDs**\nMaximilian collects role IDs whenever you add a reaction role so it can assign the correct role to users.\n\n**-User Info**\nTo provide the userinfo command, Maximilian accesses, but doesn't store, certain data about users like their roles, permissions, status, and account age.", inline=False)
-    embed.add_field(name="I want to delete my data, how do I request that?", value=f"You can delete all the data in your server by using `{bot.command_prefix}deleteall`. This will irreversibly delete all of the reaction roles and custom commands you have set up and reset the prefix to the default of `!`. Only people with the Administrator permission can use this.", inline=False)
-    await ctx.send(embed=embed)
-
-@commands.has_permissions(administrator=True)
-@bot.command()
-async def deleteall(ctx):
-    embed = discord.Embed(title="Delete all data?", description=f"You've requested that I delete all the information I have stored about this server. (see `{bot.command_prefix}privacy` for details on the data I collect)")
-    embed.add_field(name="Effects", value="If you proceed, all of the reaction roles and custom commands you've set up will be deleted, and my prefix will be reset to `!`.\n**THIS CANNOT BE UNDONE.**", inline=False)
-    embed.add_field(name="Your options", value="React with \U00002705 to proceed, or react with \U0000274c to stop the deletion process.", inline=False)
-    deletionmessage = await ctx.send(embed=embed)
-    await deletionmessage.add_reaction("\U00002705")
-    await deletionmessage.add_reaction("\U0000274c")
-    try:
-        await asyncio.sleep(1)
-        reaction = await bot.wait_for('reaction_add', timeout=120.0)
-        if str(reaction[0].emoji) == '\U00002705':
-            await ctx.send("Deleting data for this server...")
-            await ctx.trigger_typing()
-            bot.dbinst.delete(bot.database, "roles", str(ctx.guild.id), "guild_id", "", "", False)
-            bot.dbinst.delete(bot.database, "responses", str(ctx.guild.id), "guild_id", "", "", False)
-            bot.dbinst.delete(bot.database, "prefixes", str(ctx.guild.id), "guild_id", "", "", False)
-            await ctx.guild.me.edit(nick=f"[!] Maximilian")
-            await bot.responsesinst.get_responses()
-            await bot.prefixesinst.reset_prefixes()
-            embed = discord.Embed(title="\U00002705 All data for this server has been cleared!", color=discord.Color.blurple())
-            await ctx.send(embed=embed)
-        if str(reaction[0].emoji) == '\U0000274c':
-            await ctx.send("Ok. I won't delete anything.")
-    except asyncio.TimeoutError:
-        await ctx.send('Deletion request timed out.')
 
 @commands.is_owner()
 @bot.command(hidden=True)
