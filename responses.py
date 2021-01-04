6b84794d55516cdf22e2ef59eceb1f653a75b491
tk421 <colin@kolya.org>
2021-01-04 19:40:27 +0000

    remove unnecessary check

--- responses.py
+++ responses.py
@@ -18,15 +18,11 @@ class responses(commands.Cog, name='Custom Commands'):
             count = self.bot.dbinst.exec_query(self.bot.database, "select count(*) from responses where guild_id={}".format(str(guild)), False, False)
             if count is not None:
                 #if there are responses, check if there's more than one
-                if int(count['count(*)']) >= 2:
+                if int(count['count(*)']) >= 1:
                     #if so, get a list of responses and iterate over that, adding each one to the list
                     response = self.bot.dbinst.exec_query(self.bot.database, "select * from responses where guild_id={}".format(str(guild)), False, True)
                     for each in range(int(count['count(*)'])):
                         self.bot.responses.append([str(response[each]['guild_id']), response[each]['response_trigger'], response[each]['response_text']])
-                elif int(count['count(*)']) == 1:
-                    #otherwise get the one response and add it to the list
-                    response = self.bot.dbinst.exec_query(self.bot.database, "select * from responses where guild_id={}".format(str(guild)), False, False)
-                    self.bot.responses.append([str(response['guild_id']), response['response_trigger'], response['response_text']])
         return
 
     @commands.has_guild_permissions(manage_guild=True)
