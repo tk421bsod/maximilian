e6355d4f497e5911efaa4c6a3af2e682ab696ab9
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2021-01-06 16:56:21 -0800

    use commandprefix

--- misc.py
+++ misc.py
@@ -40,7 +40,7 @@ class misc(commands.Cog):
     @commands.command(help="Get some info about the bot and commands")
     async def about(self, ctx):
         embed = discord.Embed(title="About", color=discord.Color.blurple())
-        embed.add_field(name="Useful links", value=f"Use `{str(self.bot.command_prefix)}help command` for more info on a certain command. \n For more help, join the support server at https://discord.gg/PJ94gft. \n To add Maximilian to your server, with only the required permissions, click [here](https://discord.com/api/oauth2/authorize?client_id=620022782016618528&permissions=335923264&scope=bot).", inline=False)
+        embed.add_field(name="Useful links", value=f"Use `{str(self.bot.commandprefix)}help command` for more info on a certain command. \n For more help, join the support server at https://discord.gg/PJ94gft. \n To add Maximilian to your server, with only the required permissions, click [here](https://discord.com/api/oauth2/authorize?client_id=620022782016618528&permissions=335923264&scope=bot).", inline=False)
         embed.add_field(name="Fun Commands", value="Commands that have no purpose. \n `zalgo` `cats` `ping`", inline=True)
         embed.add_field(name="Other Commands", value="Commands that actually have a purpose. \n `about` `help` `userinfo` `reactionroles` `responses` `prefix` `listprefixes` `hi`", inline=True)
         print("sent some info about me")
@@ -59,11 +59,7 @@ class misc(commands.Cog):
         embed = discord.Embed(title="Maximilian Privacy Policy", color=discord.Color.blurple())
         embed.add_field(name="Why Maximilian collects data", value="Maximilian accesses/stores certain information that is necessary for certain functions (types of data collected are described below)", inline=False)
         embed.add_field(name="Data that Maximilian collects", value="**-Server IDs**\nMaximilian stores server IDs when you create a custom command, add a reaction role, or change its prefix to distinguish between different servers.\n\n**-Role IDs**\nMaximilian stores role IDs whenever you add a reaction role so it can assign the correct role to users.\n\n**-User Info**\nTo show information about users, Maximilian accesses, but doesn't store, certain data about users like their roles, permissions, status, and account age.", inline=False)
-        if ctx.prefix is not None:
-            currentprefix = self.bot.command_prefix
-        else:
-            currentprefix = ctx.prefix
-        embed.add_field(name="I want to delete my server's data, how do I request that?", value=f"You can delete all the data in your server by using{currentprefix}deleteall`. This will irreversibly delete all of the reaction roles and custom commands you have set up and reset the prefix to the default of `!`. Only people with the Administrator permission can use this.", inline=False)
+        embed.add_field(name="I want to delete my server's data, how do I request that?", value=f"You can delete all the data in your server by using{self.bot.commandprefix}deleteall`. This will irreversibly delete all of the reaction roles and custom commands you have set up and reset the prefix to the default of `!`. Only people with the Administrator permission can use this.", inline=False)
         await ctx.send(embed=embed)
 
     @commands.has_permissions(administrator=True)
@@ -71,11 +67,7 @@ class misc(commands.Cog):
     @commands.command(help="**Permanently** delete all data that Maximilian's stored about your server. (requires the Administrator permission)")
     async def deleteall(self, ctx):
         if not self.bot.waiting_for_reaction:
-            if ctx.prefix is not None:
-                currentprefix = self.bot.command_prefix
-            else:
-                currentprefix = ctx.prefix
-            embed = discord.Embed(title="Delete all data?", description=f"You've requested that I delete all the information I have stored about this server. (see `{currentprefix}privacy` for details on the data I collect)", color=discord.Color.blurple())
+            embed = discord.Embed(title="Delete all data?", description=f"You've requested that I delete all the information I have stored about this server. (see `{self.bot.commandprefix}privacy` for details on the data I collect)", color=discord.Color.blurple())
             embed.add_field(name="Effects", value="If you proceed, all of the reaction roles and custom commands you've set up will be deleted, and my prefix will be reset to `!`.\n**THIS CANNOT BE UNDONE.**", inline=False)
             embed.add_field(name="Your options", value="React with \U00002705 to proceed, or react with \U0000274c to stop the deletion process.", inline=False)
             deletionmessage = await ctx.send(embed=embed)
