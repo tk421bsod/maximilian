aec776bd4cb4d6aaaa74011f17f329ebe5d975ab
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2021-01-12 16:30:23 -0800

    move cogs to their own directory

--- main.py
+++ main.py
@@ -55,11 +55,11 @@ try:
 except pymysql.err.OperationalError:
     bot.logger.critical("Couldn't connect to database, most features won't work. Make sure you passed the right IP and that the database is configured properly.")
 #load extensions
-bot.load_extension('responses')
-bot.load_extension('prefixes')
-bot.load_extension('misc')
-bot.load_extension('reactionroles')
-bot.load_extension('userinfo')
+bot.load_extension('cogs.responses')
+bot.load_extension('cogs.prefixes')
+bot.load_extension('cogs.misc')
+bot.load_extension('cogs.reactionroles')
+bot.load_extension('cogs.userinfo')
 #create instances of certain cogs, because we need to call functions within those cogs
 bot.responsesinst = bot.get_cog('Custom Commands')
 bot.prefixesinst = bot.get_cog('prefixes')
