434ace168fb693a9e3bbd2655d0f1769dabe8cab
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2021-04-09 11:32:28 -0600

    Catch ExtensionAlreadyLoaded, log some more stuff

--- init.py
+++ init.py
@@ -31,6 +31,7 @@ class init():
             if "--ip" in args:
                 try:
                     self.bot.dbip = args[args.index("--ip")+1]
+                    self.logger.info("Set database IP address to {self.bot.dbip}")
                 except ValueError:
                     self.logger.warning("You need to specify what ip address you want to use with the database. Since you didn't specify an IP address, I'll fall back to using localhost.")
                     self.bot.dbip = "localhost"
@@ -39,12 +40,13 @@ class init():
                 self.bot.dbip = "localhost"
             if "--enablejsk" in args:
                 self.bot.load_extension("jishaku")
-                print("Loaded Jishaku.")
+                self.logger.info("Loaded Jishaku.")
         else:
             self.logger.warning("No database IP provided. Falling back to localhost.")
             self.bot.dbip = "localhost"
 
     def load_extensions(self):
+        self.logger.debug("Loading extensions...")
         extensioncount, errorcount = 0, 0
         for each in os.listdir("./cogs"):
             if each.endswith(".py"):
@@ -52,6 +54,8 @@ class init():
                     self.bot.load_extension(f"cogs.{each[:-3]}")
                     extensioncount += 1
                     self.logger.info(f"Loaded cogs.{each[:-3]}.")
+                except commands.ExtensionAlreadyLoaded:
+                    self.logger.info(f"{each[:-3]} is already loaded, skipping"
                 except commands.ExtensionFailed as error:
                     errorcount += 1
                     self.logger.error(f"{type(error.original).__name__} while loading '{error.name}'. {'Run Maximilian again with the -v command line argument to show additional information.' if type(error.original).__name__ == 'SyntaxError' else ''}")
@@ -59,11 +63,11 @@ class init():
                         self.logger.debug(traceback.format_exc())
                     elif isinstance(error.original, ModuleNotFoundError) or isinstance(error.original, ImportError):
                         self.logger.error(f"The {error.original.name} module isn't installed, '{error.name}' won't be loaded")
-        #create instances of certain cogs, because we need to call functions within those cogs
+        #create instances of certain cogs
         try:
             self.bot.coreinst = self.bot.get_cog('core')
-            self.bot.responsesinst = self.bot.get_cog('Custom Commands')
             self.bot.prefixesinst = self.bot.get_cog('prefixes')
+            self.bot.responsesinst = self.bot.get_cog('Custom Commands')
             self.bot.miscinst = self.bot.get_cog('misc')
             self.bot.reactionrolesinst = self.bot.get_cog('reaction roles')
         except:
