10cc027c39e436cd925dc78a66512228d71d448c
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2021-05-06 09:51:23 -0700

    add errors.py

---
+++ errors.py
@@ -0,0 +1,15 @@
+import discord
+
+class DeletionRequestAlreadyActive():
+    pass
+
+class DurationLimitError(discord.ext.commands.CommandError):
+    def __init__(self):
+        print("Maximum duration was exceeded.")
+
+class NoSearchResultsError(discord.ext.commands.CommandError):
+    def __init__(self):
+        print("No search results for this song.")
+
+class FileTooLargeError(discord.ext.commands.CommandError):
+    pass
