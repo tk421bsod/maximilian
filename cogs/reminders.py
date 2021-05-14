8c37587cf625c5162c5812e615fe728ccfd8b033
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2021-05-14 11:04:10 -0700

    return after deletion request finishes

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -129,7 +129,7 @@ class reminders(commands.Cog):
             return
         if action == "deleteall":
             try:
-                await core.deletion_request(self.bot).create_request("todo", ctx)
+                return await core.deletion_request(self.bot).create_request("todo", ctx)
             except errors.DeletionRequestAlreadyActive:
                 await ctx.send("A deletion request is already active.")
         if action == "list" or entry == None:
