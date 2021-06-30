ac448d84dfa60cb688bf44c766e6ab74a4d3ae5a
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2021-06-29 19:55:55 -0700

    call get_prefix instead of using command_prefix

--- helpcommand.py
+++ helpcommand.py
@@ -29,7 +29,7 @@ class HelpCommand(commands.HelpCommand):
                 name = cog.qualified_name
                 filtered = await self.filter_commands(commands, sort=True)
                 if filtered:
-                    value = '\u2002 '.join(f'`{self.context.bot.command_prefix}' + c.name + '`' for c in commands if not c.hidden)
+                    value = '\u2002 '.join(f'`{await self.context.bot.coreinst.get_prefix(self.context.bot, self.context.message)}' + c.name + '`' for c in commands if not c.hidden)
                     if cog and cog.description:
                         value = '{0}\n{1}'.format(cog.description, value)
 
