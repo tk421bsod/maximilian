b18a9dbc0afbd257241b9ec17b28c20be1f0e7ca
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2021-09-23 15:27:36 -0700

    add setup instructions, python3.7 compat

--- cogs/reactionroles.py
+++ cogs/reactionroles.py
@@ -2,65 +2,38 @@ import typing
 
 import discord
 from discord.ext import commands
-import typing
-import logging
-import traceback
-import pymysql
+
 
 class reactionroles(commands.Cog, name="reaction roles"):
     '''Reaction role commands'''
-    def __init__(self, bot, load=False):
+    def __init__(self, bot):
         self.bot = bot
-        self.bot.reaction_roles = {}
-        self.logger = logging.getLogger('cogs.reactionroles')
-        if load:
-            self.bot.loop.create_task(self.update_reactionroles_cache())
-
-    async def update_reactionroles_cache(self):
-        await self.bot.wait_until_ready()
-        self.logger.info("Updating reaction roles cache...")
-        temp_reaction_roles = {}
-        for guild in self.bot.guilds:
-            rolesdata = self.bot.dbinst.exec_query(self.bot.database, "select role_id, message_id, emoji from roles where guild_id = %s", (guild.id), fetchall=True)
-            if not rolesdata:
-                continue
-            temp_reaction_roles[guild.id] = []
-            for row in rolesdata:
-                temp_reaction_roles[guild.id].append(row)
-        self.bot.reaction_roles = temp_reaction_roles
-        self.logger.info("Updated reaction roles cache!")
 
     @commands.command(help="Add, remove, or list reaction roles, only works if you have the 'Manage Roles' permission. This command takes 4 arguments (1 optional), action (the action to perform, either `add`, `delete`, or `list`), role (a role, you can either mention it or provide the id), messageid (the id of the message you want people to react to), and emoji (the emoji you want people to react with, it must be in a server Maximilian is in or a default emoji, this can be blank if you want people to react with any emoji)", aliases=['reactionrole'])
     @commands.has_guild_permissions(manage_roles=True)
-    async def reactionroles(self, ctx, action, role : typing.Optional[discord.Role]=None, emoji : typing.Optional[typing.Union[discord.PartialEmoji, str]]=None):
-        if role != None and ctx.message.reference != None:
+    async def reactionroles(self, ctx, action, role : typing.Optional[discord.Role]=None, messageid : typing.Optional[int]=None, emoji : typing.Optional[typing.Union[discord.PartialEmoji, str]]=None):
+        if role != None and messageid != None:
             if action == "add":
-                messageid = ctx.message.reference.message_id
-                try:
-                    self.bot.dbinst.exec_query(self.bot.database, "insert into roles values(%s, %s, %s, %s)", (ctx.guild.id, role.id, messageid, str(emoji)), noreturn=True)
-                    await self.update_reactionroles_cache()
+                if self.bot.dbinst.insert(self.bot.database, "roles", {"guild_id" : str(ctx.guild.id), "role_id" : str(role.id), "message_id" : str(messageid), "emoji" : str(emoji)}, "role_id", False, "", False, "", False) == "success":
+                    print("added a reaction role")
                     await ctx.send("Added a reaction role.")
-                except pymysql.err.IntegrityError: 
-                    traceback.print_exc()
+                else: 
                     await ctx.send("Failed to add a reaction role, there might be a duplicate. Try deleting the reaction role you just tried to add.")
-                except:
-                    await ctx.send("There was an error while adding a reaction role. Try again.")
             if action == "delete":
                 print("deleted a reaction role")
-                if self.bot.dbinst.exec_query(self.bot.database, "delete from roles where role_id = %s", (role.id)) == "successful":
+                if self.bot.dbinst.delete(self.bot.database, "roles", str(role.id), "role_id", "", "", False) == "successful":
                     await ctx.send("Deleted a reaction role.")
                 else:
                     await ctx.send(f"Failed to delete a reaction role, are there any reaction roles set up for role id '{str(role.id)}'? Try using '{str(await self.bot.get_prefix(ctx.message))}'reactionroles list all all' to see if you have any reaction roles set up.")
         elif action == "list":
-            roles = self.bot.dbinst.exec_query(self.bot.database, "select * from roles where guild_id=%s", (ctx.guild.id), fetchall=True)
+            roles = self.bot.dbinst.exec_query(self.bot.database, "select * from roles where guild_id={}".format(ctx.guild.id), False, True)
             reactionrolestring = ""
-            if roles != "()": 
-                reactionrolestring = f"{reactionrolestring} message id:  {str(each['message_id'])}  role id:  {str(each['role_id'])}  role name:  {discord.utils.get(ctx.guild.roles, id=int(each['role_id'])).name}  emoji:  {str(each['emoji'])}, "        
+            if roles != "()":
+                for each in roles: 
+                    reactionrolestring = f"{reactionrolestring} message id:  {str(each['message_id'])}  role id:  {str(each['role_id'])}  role name:  {discord.utils.get(ctx.guild.roles, id=int(each['role_id'])).name}  emoji:  {str(each['emoji'])}, "        
                 print("listed reaction roles")
                 await ctx.send("reaction roles: " + str(reactionrolestring[:-2]))
-        elif ctx.message.reference == None:
-            return await ctx.send("You need to reply to the message you want to add the reaction role to.")
-        elif role == None:
+        elif role == None or messageid == None:
             await ctx.send(f"It doesn't look like you've provided all of the required arguments. See `{await self.bot.get_prefix(ctx.message)}help reactionroles` for more details.")
             return
 
@@ -112,10 +85,10 @@ class reactionroles(commands.Cog, name="reaction roles"):
                         print("removed role from user")
                         await ctx.send(f"Removed the '{role.name}' role from <@!{member.id}>.", delete_after=5)
                         return
-                    await ctx.send(f"<@!{member.id}> For some reason, you don't have the '{role.name}' role, even though you reacted to this message. Try removing your reaction and adding your reaction again. If this keeps happening, notify tk421#2016.", delete_after=15)
+                    await ctx.send(f"<@!{member.id}> For some reason, you don't have the '{role.name}' role, even though you reacted to this message. Try removing your reaction and adding your reaction again. If this keeps happening, notify tk421#7244.", delete_after=15)
 
 def setup(bot):
-    bot.add_cog(reactionroles(bot, True))
+    bot.add_cog(reactionroles(bot))
 
 def teardown(bot):
     bot.remove_cog(reactionroles(bot))
---
+++ setup.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+#uwu formatting
+#these might not display correctly on some terminals 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+if [ "$1" == "remote" ];
+then
+    ip="%"
+else
+    ip="localhost"
+fi
+
+if [ "$1" == "nodb" ];
+then
+    nodb='true'
+else
+    nodb='false'
+fi
+
+echo "${bold}----------------Maximilian Setup----------------${normal}"
+echo "This script automates most parts of setting up Maximilian. You'll still need to do a few things, like getting the token and choosing a password for the database."
+if [ "$1" == "remote" ];
+then
+    echo "${bold}You've chosen to configure the database for remote access.${normal} This will not set up Maximilian on this computer."
+elif [ "$1" == "nodb" ];
+then
+    echo "${bold}You've chosen to not set up the database on this computer.${normal} If you've set up the database on a different computer, run main.py with '--ip <ip>', <ip> being the ip address of that other computer."
+    echo "This will still set up Maximilian on this computer."
+else 
+    echo "${bold}You didn't pass any arguments, so the database will only be configured for local access.${normal} Maximilian will still be set up."
+fi
+echo "This script installs several packages (and also downloads some code) so ${bold}you'll need a stable Internet connection${normal}."
+echo "You can press ${bold}Ctrl-C${normal} or ${bold}Ctrl-Z${normal} to quit at any time."
+echo "${bold}Read the README.md (and HOSTING.md) for Maximilian if you haven't already, because a README isn't any good if you don't read it.${normal}"
+echo ""
+read -p "Have you read the README? Y/N  " readme 
+if [ ${readme^^} == 'N' ];
+then
+    echo "Go read the README!"
+    exit 1
+elif [ ${readme^^} == 'Y' ];
+then
+    echo "Great. Continuing with setup in a few seconds..."
+    sleep 5
+else
+    echo "what?"
+    exit 1
+fi
+echo ""
+if [ ! -f "token.txt" -a ! $ip == '%' ];
+then
+    echo "It doesn't look like you've put a token in token.txt."
+    echo "Enter the token you want Maximiian to use. If you don't know what this is, create an application in Discord's Developer Portal, create a bot account for that application, and copy the account's token. Then paste it here."
+    echo "Your input will be hidden to keep the token secret."
+    read -s token
+    if [ "$token" == "" -o "$token" == " " ];
+    then 
+        echo "You have to actually enter a token."
+        exit 13
+    fi
+    echo "$token" >> token.txt
+    echo "Alright, saved the token to 'token.txt'. If the token isn't valid, either edit the file and add the correct token or delete the file and run this again."
+    echo ""
+fi
+
+echo "Enter the password you want to use for the database."
+if [ $nodb == 'false' ];
+then
+    echo "The database and maximilian will be configured to use this password."
+else
+    echo "Make sure it's the password 'maximilianbot' logs in with."
+fi
+echo "Your input will be hidden to keep the password secret."
+read -s password
+if [ "$password" == "" -o "$password" == " " ];
+then
+    echo "The password can't be blank!"
+    echo "Run this again and enter an actual password."
+    exit 12
+fi
+echo "Type the same password again."
+read -s passwordconf
+if [ "$password" != "$passwordconf" ];
+then
+    echo "The passwords don't match! Run this again."
+    echo "Remember that passwords are case-sensitive."
+    exit 11
+fi  
+echo ""
+echo "Ok, the password has been set. "
+if [ ! $ip == '%' ];
+then
+    echo "It'll be saved to 'dbp.txt' after setting up the database."
+fi
+echo ""
+echo "Getting latest packages..."
+sudo apt update
+echo ""
+echo "Installing required packages..."
+packages="python3 python3-pip ffmpeg g++"
+if [ $nodb == 'false' ];
+then
+    packages+=" mariadb-server"
+fi
+sudo apt -y install $packages
+echo ""
+if [ $nodb == 'false' ];
+then
+    echo "Setting up the database..."
+    sudo service mysql start
+    sudo mysql -Be "CREATE DATABASE maximilian; CREATE USER 'maximilianbot'@'$ip' IDENTIFIED BY '$password'; GRANT INSERT, SELECT, UPDATE, CREATE, DELETE ON maximilian.* TO 'maximilianbot'@'$ip' IDENTIFIED BY '$password'; FLUSH PRIVILEGES;"
+else
+    echo "Not setting up the database."
+fi
+if [ -f "dbp.txt" ];
+then
+    rm -f dbp.txt
+fi
+if [ ! $ip == '%' ];
+then
+    echo "$password" >> dbp.txt
+    echo "Saved the password to dbp.txt"
+    echo ""
+    echo "Installing dependencies..."
+    pip3 install -r requirements.txt
+    echo ""
+    echo "Alrighty, Maximilian should be (almost) fully installed now. Try running it using 'python3 main.py --enablejsk -i'. If you want cogs.images and cogs.misc's bottomify command to work, use 'pip install -r requirements-extra.txt'."
+else
+    echo "Finished setting up the database."
+fi
