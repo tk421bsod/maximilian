1899f1515513339090905c3fddf156d80ba59328
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2023-01-16 23:24:53 -0800

    move some startup stuff to different file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #main.py: loads core libraries and everything in the cogs folder, then starts Maximilian
-
 import sys
+
+if sys.version_info.major == 3 and sys.version_info.minor < 7:
+    print("Hi there. It looks like you're trying to run maximilian with an older version of Python 3.")
+    print("Maximilian cannot run on Python versions older than 3.7.")
+    print("You'll need to upgrade Python to continue.")
+    quit()
+
 
 if "--help" in sys.argv:
     print("main.py usage: python3 main.py [OPTIONS]\n")
@@ -41,6 +47,7 @@ try:
     import core
     import db
     import settings
+    import startup
     from updater import update
 except ImportError as e:
     print("Maximilian cannot start because a required component failed to load.\nTry running 'pip3 install -U -r requirements.txt' and ensuring Maximilian is using the correct Python installation.\nHere's some more error info:")
@@ -55,88 +62,6 @@ if not "--no-rich" in sys.argv:
         print("Not enabling rich text - import failed")
 else:
     print("Not enabling rich text - user requested")
-
-def parse_version(versionstring):
-    version = common.Version()
-    version.major, version.minor, version.patch = [int(i) for i in versionstring.replace('a','').split('.')[:3]]
-    return version
-
-def check_version():
-    if parse_version(discord.__version__).major < 2:
-        print("\nMaximilian no longer supports discord.py versions below 2.0.")
-        print("Either update discord.py (through 'python3 -m pip install discord.py[voice]') or use a Maximilian version below 1.0.")
-        print("If you choose to use an old version of Maximilian, you're on your own - those versions lack support from tk421 and compatibility with discord.py 2. Old versions may also stop working without notice.")
-        print("See https://gist.github.com/Rapptz/c4324f17a80c94776832430007ad40e6 for more information about this.")
-        quit()
-
-def initialize_i18n(bot):
-    bot.logger.info('Initializing i18n...')
-    if '--language' in sys.argv:
-        language = sys.argv[sys.argv.index('--language')+1]
-        supported = [i.split('.')[0] for i in os.listdir('languages') if i.endswith('.txt')]
-        if language not in supported:
-            bot.logger.error(f"That language isn't supported right now. The only supported languages are {supported}")
-            sys.exit(25)
-    else:
-        bot.logger.info("No language specified, defaulting to en")
-        language = 'en'
-    bot.logger.info(f"Set language to {language}")
-    with open(f'languages/{language}', 'r') as data:
-        bot.i18n = json.load(data)
-    bot.logger.info('Initialized i18n, everything after this message will use the language above')
-
-
-def config_logging(args):
-    """Sets logging level and file to write to"""
-    #mapping of argument to logging level and status message
-    levelmapping = {"-v":[logging.DEBUG, "Debug logging enabled."], "--debug":[logging.DEBUG, "Debug logging enabled."], "--verbose":[logging.DEBUG, "Debug logging enabled."], "-i":[logging.INFO, "Logging level set to INFO."], "--info":[logging.INFO, "Logging level set to INFO"], "-w":[logging.WARN, "Logging level set to WARN."], "--warn":[logging.WARN, "Logging level set to WARN."], "-e":[logging.ERROR, "Logging level set to ERROR."], "--error":[logging.ERROR, "Logging level set to ERROR."], "-q":["disable", "Logging disabled. Tracebacks will still be shown in the console, along with a few status messages."], "--quiet":["disable", "Logging disabled. Tracebacks will still be shown in the console, along with a few status messages."]}
-    try:
-        _handlers = [RichHandler(rich_tracebacks=True, tracebacks_suppress=[discord, pymysql])]
-    except NameError: #rich wasn't imported, use stdout instead
-        _handlers = [logging.StreamHandler(sys.stdout)]
-    if os.path.isdir('logs'):
-        _handlers.append(logging.FileHandler(f"logs/maximilian-{datetime.date.today()}.log"))
-    else:
-        print("The 'logs' directory doesn't exist! Not logging to a file.")
-    for key, value in levelmapping.items():
-        if key not in args:
-            pass
-        elif key != "-q" and key != "--quiet":
-            logging.basicConfig(level=value[0], handlers=_handlers)
-            print(value[1])
-            logging.getLogger("maximilian.config_logging").warning(f"Logging started at {datetime.datetime.now()}")
-            return
-        else:
-            logging.disable()
-            print(value[1])
-            return
-    try:
-        RichHandler()
-        logging.basicConfig(level=logging.WARN, handlers=_handlers, format="%(message)s", datefmt="[%X]")
-    except NameError: #rich not imported
-        logging.basicConfig(level=logging.WARN, handlers=_handlers)
-    print("No logging level specified, falling back to WARN.")
-    logging.getLogger("maximilian.config_logging").warning(f"Logging started at {datetime.datetime.now()}")
-
-def parse_arguments(bot, args):
-    if len(args) > 1:
-        if "--ip" in args:
-            try:
-                bot.dbip = args[args.index("--ip")+1]
-                bot.logger.info(f"Set database IP address to {bot.dbip}.")
-            except ValueError:
-                bot.logger.warning("You need to specify what ip address you want to use with the database. Since you didn't specify an IP address, I'll fall back to using localhost.")
-                bot.dbip = "localhost"
-        else:
-            bot.logger.warning("No database IP address provided. Falling back to localhost.")
-            bot.dbip = "localhost"
-        if "--noload" in args:
-            bot.noload = [i for i in args[args.index('--noload')+1:] if not i.startswith('-')]
-        else:
-            bot.noload = []
-    else:
-        bot.logger.warning("No arguments provided.")
-        bot.dbip = "localhost"
 
 async def load_extensions_async(bot):
     """New non-blocking method for loading extensions. Same functionality as load_extensions but compatible with dpy2."""
@@ -223,7 +148,7 @@ async def run(logger):
         subprocess.run("echo \"jsk_used:1\" >> config", shell=True)
     token = config['token']
     logger.debug("Checking discord.py version...")
-    check_version()
+    startup.check_version()
     intents = discord.Intents.default()
     intents.members=True
     intents.message_content = True
@@ -248,7 +173,7 @@ async def run(logger):
     #parse additional arguments (ip, enablejsk, noload)
     bot.noload = []
     bot.logger.debug("Parsing command line arguments...")
-    parse_arguments(bot, sys.argv)
+    startup.parse_arguments(bot, sys.argv)
     bot.guildlist = []
     bot.prefixes = {}
     bot.responses = []
@@ -285,7 +210,7 @@ async def run(logger):
 print("Starting Maximilian...\nPress Ctrl-C at any time to quit.\n")
 print("setting up logging...")
 # set a logging level
-config_logging(sys.argv)
+startup.config_logging(sys.argv)
 outer_logger = logging.getLogger(f'maximilian') #different name than inside run for readability
 # noinspection PyBroadException
 try:
