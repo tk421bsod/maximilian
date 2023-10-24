3807e1391315adcc7e2fca49ee022ab523ccf6b2
tk421bsod <42457203+TK421bsod@users.noreply.github.com>
2023-10-24 12:12:25 -0700

    cogs.config - document

--- cogs/config.py
+++ cogs/config.py
@@ -6,10 +6,15 @@ class settings(commands.Cog):
 
     @commands.command()
     async def config(self, ctx, category:str=None, *, setting:str=None):
-        '''Change Maximilian\'s settings'''
+        """
+        Change Maximilian's settings.
+        Settings are organized into categories. 
+        View a list of categories by running this without any arguments.
+        View a list of settings by specifying a category.
+        Toggle a setting by specifying a category then a setting.
+        """
         #reading the source code? this cog is just a wrapper for the config method(s) in settings.py.
         await self.bot.settings.config(ctx, category, setting=setting)
-
 
 async def setup(bot):
     await bot.add_cog(settings(bot))
